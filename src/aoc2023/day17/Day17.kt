8ebfa4aef0b98b96b90c36e1f2a417ad244fc1ef
Joost Yervante Damad <joost@damad.be>
2023-12-23 18:32:29 +0100

    simplification

--- src/aoc2023/day17/Day17.kt
+++ src/aoc2023/day17/Day17.kt
@@ -25,12 +25,12 @@ private fun parse(input: List<String>): Map {
     return Map(cells)
 }
 
-private data class WrappedCell(val cell: Cell, val lastDirectionSteps: Int?) {
+private data class WrappedCell(val cell: Cell, val direction: Direction?, val lastDirectionSteps: Int?) {
     override fun equals(other: Any?): Boolean {
         return when (other) {
             is WrappedCell -> {
                 if (lastDirectionSteps != null && other.lastDirectionSteps != null) {
-                    lastDirectionSteps == other.lastDirectionSteps && cell == other.cell
+                    lastDirectionSteps == other.lastDirectionSteps && cell == other.cell && direction == other.direction
                 } else {
                     cell == other.cell
                 }
@@ -47,46 +47,37 @@ private data class WrappedCell(val cell: Cell, val lastDirectionSteps: Int?) {
 }
 
 private fun search(map: Map): Int {
-    val start = WrappedCell(map.at(0, 0)!!, 0)
-    val end = WrappedCell(map.at(map.xSize - 1, map.ySize - 1)!!, null)
+    val start = WrappedCell(map.at(0, 0)!!, null, 0)
+    val end = WrappedCell(map.at(map.xSize - 1, map.ySize - 1)!!, null, null)
     fun cost(c: WrappedCell, d: WrappedCell) = d.cell.loss + (d.lastDirectionSteps ?: 0)
     fun neighbours(c: WrappedCell, visited: List<WrappedCell>): List<WrappedCell> {
         // if only one, visited is our starting cell, so just take all the neighbours
         if (visited.size == 1) {
-            return map.neighbours(c.cell).map { WrappedCell(it.second, 1) }
+            return map.neighbours(c.cell).map { WrappedCell(it.second, it.first, 1) }
         }
-        val s1 = visited.subList(0, visited.size - 1)
-        val s2 = visited.subList(1, visited.size)
-        val directions = s1.zip(s2).map { it.first.cell.location.direction(it.second.cell.location) }
-        val lastDirection = directions.last()
+        val lastDirection = c.direction!!
         // we can't make a U-turn
         val forbiddenDirections = mutableListOf(-lastDirection)
         // if we ended with moving 3 times in the same direction, we can't continue in the same direction
-        val needToTurn = if (directions.size >= 3) {
-            val lastDirections = directions.takeLast(3).distinct()
-            lastDirections.size == 1
-        } else {
-            false
-        }
-        if (needToTurn) {
+        if (c.lastDirectionSteps == 3) {
             forbiddenDirections.add(lastDirection)
         }
         return map.neighbours(c.cell).filter { it.first !in forbiddenDirections }.map {
             if (it.first != lastDirection) {
-                WrappedCell(it.second, 1)
+                WrappedCell(it.second, it.first, 1)
             } else {
-                WrappedCell(it.second, c.lastDirectionSteps!! + 1)
+                WrappedCell(it.second, lastDirection, c.lastDirectionSteps!! + 1)
             }
         }
     }
-    fun heuristic(c: Cell): Int {
-        return map.xSize - c.location.x + map.ySize - c.location.y
+    fun heuristic(c: WrappedCell): Int {
+        return map.xSize - c.cell.location.x + map.ySize - c.cell.location.y
     }
     fun show(visited: List<Cell>) {
         map.show(visited)
     }
 
-    var path = AStar.path(start, end, ::cost, ::neighbours, show = {})
+    val path = AStar.path(start, end, ::cost, ::neighbours, show = {})
     map.show(path.map { it.cell })
     val loss = path.sumOf { it.cell.loss } - start.cell.loss
     return loss
@@ -109,6 +100,6 @@ fun main() {
         map.show()
         val res = search(map)
         println(res)
-        check(102 == res)
+        check(1054 == res)
     }
 }
