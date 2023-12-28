cbcc1d1a9b2b15c50c99d510f298eef8fe234c50
Joost Yervante Damad <joost@damad.be>
2023-12-28 20:29:47 +0100

    day 13

--- src/aoc2023/day13/Day13.kt
+++ src/aoc2023/day13/Day13.kt
@@ -1,73 +1,35 @@
-package be.damad.aoc2023.aoc13
+package aoc2023.day13
 
+import day
+import util.*
 import kotlin.math.min
 
-private val testData = """#.##..##.
-..#.##.#.
-##......#
-##......#
-..#.##.#.
-..##..##.
-#.#.##.#.
+@JvmInline
+private value class Cell(val value: Boolean): WithChar {
+    override val c get() = if (value) { '#' } else { '.' }
 
-#...##..#
-#....#..#
-..##..###
-#####.##.
-#####.##.
-..##..###
-#....#..#""".split('\n')
-
-private val testData2 = """#.#.####.#.#.##
-.#.##..##.#...#
-.#..#..#..#.#..
-.##..##..##.#.#
-....####.......
-##...##...##..#
-##...##...##..#
-....####.......
-.##..##..##.#.#
-.#..#..#..#.#..
-.#.##..##.#...#
-#.#.####.#.#.#.
-..#.#..#.#....#
-##.#.##.#.####.
-..#..##..#....#
-#..#.##.#..#.##
-#.#.####.#.#...""".split('\n')
-
-private data class Cell(val x: Int, val y: Int, val rock: Boolean) {
-    override fun toString() = if (rock) { "#" } else { "." }
 }
-private data class Map(val cells: List<List<Cell>>) {
-    fun at(x: Int, y: Int) = cells.getOrNull(y)?.getOrNull(x)
 
-    fun row(y: Int) = cells.getOrNull(y)?.map { it.rock }
-    fun column(x: Int) = cells.mapNotNull { it.getOrNull(x)?.rock }
-    fun smudge(x: Int, y: Int): Map {
-        return copy(cells = cells.mapIndexed { yIndex, row ->
-            if (yIndex == y) {
-                row.mapIndexed { xIndex, cell ->
-                    if (xIndex == x) {
-                        cell.copy(rock = !cell.rock)
-                    } else {
-                        cell
-                    }
+
+private typealias Map = ItemMaze<Cell>
+
+private fun Map.row(y: Int) = rows.getOrNull(y)?.map { it.t }
+private fun Map.column(x: Int) = rows.mapNotNull { it.getOrNull(x)?.t }
+
+private fun Map.smudge(x: Int, y: Int): Map {
+    return copy(rows = rows.mapIndexed { yIndex, row ->
+        if (yIndex == y) {
+            row.mapIndexed { xIndex, cell ->
+                if (xIndex == x) {
+                    cell.copy(t = Cell(!cell.t.value))
+                } else {
+                    cell
                 }
-            } else {
-                row.toList()
             }
-        })
-    }
-
-    fun print() {
-        for (row in cells) {
-            val l = row.joinToString("") { "$it" }
-            println(l)
+        } else {
+            row.toList()
         }
-        println()
-    }
-
+    })
 }
 
 private fun parse(data: List<String>): List<Map> {
@@ -76,22 +38,22 @@ private fun parse(data: List<String>): List<Map> {
     var y = 0
     for (line in data) {
         if (line.isEmpty()) {
-            res.add(Map(cells.toList()))
+            res.add(makeItemMaze(cells.toList()))
             cells.clear()
             y = 0
         } else {
-            cells.add(line.mapIndexed { x, c -> Cell(x, y, c == '#') })
+            cells.add(line.mapIndexed { x, c -> Cell(c == '#') })
             y++
         }
     }
-    res.add(Map(cells))
+    res.add(makeItemMaze(cells))
     return res
 }
 
 private fun checkSplitY(map: Map, y1: Int, y2: Int): Boolean {
     var y1 = y1
     var y2 = y2
-    while (y1 > 0 && y2 < map.cells.size - 1) {
+    while (y1 > 0 && y2 < map.ySize - 1) {
         y1--
         y2++
         if (map.row(y1) != map.row(y2)) {
@@ -104,7 +66,7 @@ private fun checkSplitY(map: Map, y1: Int, y2: Int): Boolean {
 private fun checkSplitX(map: Map, x1: Int, x2: Int): Boolean {
     var x1 = x1
     var x2 = x2
-    while (x1 > 0 && x2 < map.cells[0].size - 1) {
+    while (x1 > 0 && x2 < map.xSize - 1) {
         x1--
         x2++
         if (map.column(x1) != map.column(x2)) {
@@ -115,8 +77,8 @@ private fun checkSplitX(map: Map, x1: Int, x2: Int): Boolean {
 }
 
 private fun summarizeY(map: Map): Int {
-    val r1 = 0 until map.cells.size - 1
-    val r2 = 1 until map.cells.size
+    val r1 = 0 until map.ySize - 1
+    val r2 = 1 until map.ySize
     for (pair in r1.zip(r2)) {
         if (map.row(pair.first) == map.row(pair.second)) {
             if (checkSplitY(map, pair.first, pair.second)) {
@@ -129,8 +91,8 @@ private fun summarizeY(map: Map): Int {
 }
 
 private fun summarizeX(map: Map): Int {
-    val r1 = 0 until map.cells[0].size - 1
-    val r2 = 1 until map.cells[0].size
+    val r1 = 0 until map.xSize - 1
+    val r2 = 1 until map.xSize
     for (pair in r1.zip(r2)) {
         if (map.column(pair.first) == map.column(pair.second)) {
             if (checkSplitX(map, pair.first, pair.second)) {
@@ -142,27 +104,27 @@ private fun summarizeX(map: Map): Int {
 }
 
 private fun calculate2(map: Map): Int {
-    map.print()
+    map.show()
     val oldX = summarizeX(map)
     val oldY = summarizeY(map)
     var m = Int.MAX_VALUE
     var m2 = Int.MAX_VALUE
     var ySeen = false
-    for (y in map.cells.indices) {
-        for (x in map.cells[0].indices) {
+    for (y in map.yIndices) {
+        for (x in map.xIndices) {
             val s: Map = map.smudge(x, y)
             val y2 = summarizeY(s)
-            val edgeDiff = min(y2, map.cells.size - y2)
+            val edgeDiff = min(y2, map.ySize - y2)
             val validRange = (y2 - edgeDiff)..(y2 + edgeDiff - 1)
             if (y2 != 0 && y2 != oldY && y in validRange) {
                 println("smudge at ($x,$y) y-mirror at $y2")
-                s.print()
+                s.show()
                 return y2 * 100
             }
             val x2 = summarizeX(s)
             if (x2 != 0 && x2 != oldX) {
                 println("smudge at ($x,$y) x-mirror at $x2")
-                s.print()
+                s.show()
                 return x2
             }
 //            if (!ySeen) {
@@ -192,40 +154,25 @@ private fun calculate2(map: Map): Int {
 
 private fun calculate(map: Map) = summarizeX(map) + 100 * summarizeY(map)
 
-private fun calculate(data: List<Map>): Int {
+private fun part1(data: List<Map>): Int {
     val c = data.map { calculate(it) }
     println(c)
     return c.sum()
 }
 
-private fun calculate2(data: List<Map>): Int {
+private fun part2(data: List<Map>): Int {
     val c = data.map { calculate2(it) }
     println(c)
     return c.sum()
 }
 
 fun main() {
-    run {
-        val res = calculate(parse(testData))
-        println(res)
-        check(405 == res)
-    }
+    day(2023, 13) {
+        part1(405, "test") { part1(parse(it)) }
+        part1(36448, "input") { part1(parse(it)) }
+        // TODO: part2
+        part2(400, "test") { part2(parse(it)) }
+        part2(36448, "input") { part2(parse(it)) }
 
-    run {
-        val res = calculate2(parse(testData))
-        println(res)
-        check(400 == res)
-    }
-
-    run {
-        val res2 = calculate(parse(aoc13data))
-        println(res2)
-        check(36448 == res2)
-    }
-
-    run {
-        val res2 = calculate2(parse(aoc13data))
-        println(res2)
-        check(36448 == res2)
     }
 }
